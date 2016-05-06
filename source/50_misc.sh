09f77365c37d7680039f3c87b478e249674e1dbf
Samuel Parkinson <samuel.parkinson@ft.com>
2016-05-06 16:56:09 +0100

    Update the ssh autocomplete to manage hosts and ip addresses.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -20,7 +20,7 @@ function titlebar () {
 
 # SSH auto-completion based on entries in known_hosts.
 if [[ -e ~/.ssh/known_hosts ]]; then
-    complete -o default -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh scp sftp
+    complete -o default -W "$(echo `cat ~/.ssh/known_hosts | cut -d ' ' -f 1 | tr ',' "\n" | uniq`;)" ssh scp sftp
 fi
 
 # Disable ansible cows }:]

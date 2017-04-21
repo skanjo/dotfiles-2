21a842531d82e91ecc45ccc4649b7711a6b63010
Samuel Parkinson <samuel.parkinson@ft.com>
2017-04-21 16:40:50 +0100

    Add vault autocomplete and limit clean history to vault write.

--- source/50_vault.sh
+++ source/50_vault.sh
@@ -5,5 +5,49 @@ export VAULT_AUTH_GITHUB_TOKEN="$(security find-generic-password -a ${USER} -s v
 
 function vault () {
   command vault "$@"
-  history -d $((HISTCMD-1))
+  if [[ $1 == write ]]; then history -d $((HISTCMD-1)) &> /dev/null; fi
 }
+
+function __vault_mounts() {
+  (
+    set -euo pipefail
+    if ! vault mounts 2> /dev/null | awk 'NR > 1 {print $1}'; then
+      echo "secret"
+    fi
+  )
+}
+
+function __vault_complete() {
+  local VAULT_COMMANDS="$(vault 2>&1 | egrep '^ +' | awk '{print $1}')"
+
+  local cur
+  local prev
+
+  if [ $COMP_CWORD -gt 0 ]; then
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+  fi
+
+  local line=${COMP_LINE}
+
+  if [[ $prev =~ ^(policies|policy-write|policy-delete) ]]; then
+    local policies=$(vault policies 2> /dev/null)
+    COMPREPLY=($(compgen -W "$policies" -- $cur))
+  elif [ "$(echo $line | wc -w)" -le 2 ]; then
+    if [[ "$line" =~ ^vault\ (read|write|delete|list)\ $ ]]; then
+      COMPREPLY=($(compgen -W "$(__vault_mounts)" -- ''))
+    else
+      COMPREPLY=($(compgen -W "$VAULT_COMMANDS" -- $cur))
+    fi
+  elif [[ "$line" =~ ^vault\ (read|write|delete|list)\ (.*)$ ]]; then
+    path=${BASH_REMATCH[2]}
+    if [[ "$path" =~ ^([^ ]+)/([^ /]*)$ ]]; then
+      list=$(vault list -format=yaml ${BASH_REMATCH[1]} 2> /dev/null | awk '{ print $2 }')
+      COMPREPLY=($(compgen -W "$list" -P "${BASH_REMATCH[1]}/" -- ${BASH_REMATCH[2]}))
+    else
+      COMPREPLY=($(compgen -W "$(__vault_mounts)" -- $path))
+    fi
+  fi
+}
+
+complete -o default -o nospace -F __vault_complete vault
